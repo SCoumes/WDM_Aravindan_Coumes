6d84aa184daf0e60cff7b3f1b45475677fcfe4bd
PriyankaAravindan <51697830+PriyankaAravindan@users.noreply.github.com>
2022-03-01 17:06:19 +0100

    Add files via upload

--- src/QueryFunction.py
+++ src/QueryFunction.py
@@ -9,33 +9,27 @@ def queryFunction(title, author):
     }
     search = GoogleSearch(params)
     results = search.get_dict()
-    if('organic_results' in results.keys()):
-        if('publication_info' in results['organic_results'][0].keys()):
-            if('authors' in  results['organic_results'][0]['publication_info'].keys()):
-                    Author_Ids = results['organic_results'][0]['publication_info']['authors']
-    
-                    for each in Author_Ids:
-                        intermediate = {}
-                        params = {
-                          "engine": "google_scholar_author",
-                          "author_id": each['author_id'],
-                          "api_key": "255dc7fe9dd992e9626160a1d48557435eacc85f8445f9bbf0d624142d9cfd8d"
-                        }
-                        
 
-                        search = GoogleSearch(params)
-                        results = search.get_dict()
-                        if( 'affiliations' in results['author'].keys()):
-                            intermediate['name'] = each['name']
-                            intermediate['affiliations'] = results['author']['affiliations']
-                            Institutes[each['author_id']]  =  intermediate
-                        else:
-                            return None
-            else:
-                return None
-        else:
-            return None
-    else:
-        return None
-    return Institutes
-#profiles = results["profiles"]
+    try: 
+        Author_Ids = results['organic_results'][0]['publication_info']['authors']
+
+        for each in Author_Ids:
+            intermediate = {}
+            params = {
+              "engine": "google_scholar_author",
+              "author_id": each['author_id'],
+              "api_key": "255dc7fe9dd992e9626160a1d48557435eacc85f8445f9bbf0d624142d9cfd8d"
+            }
+
+
+            search = GoogleSearch(params)
+            results = search.get_dict()
+            try:
+                intermediate['name'] = each['name']
+                intermediate['affiliations'] = results['author']['affiliations']
+                Institutes[each['author_id']]  =  intermediate
+                return Institutes
+            except KeyError:
+                pass
+    except KeyError:
+        pass
