c0275e425d5007b314606c30c985bcd2b514625b
Simon Coumes <simon.coumes@ens-rennes.fr>
2022-03-01 23:55:06 +0100

    Incremental bugfix.

--- src/ringsCitations.py
+++ src/ringsCitations.py
@@ -46,7 +46,7 @@ def getRingsFromTable(citationCountTable) :
 def putListsOnDisk(citationCountTables, targetpath) :
     count = 0
     for table in citationCountTables :
-        fileList = getRingsFromTable(citationCountTables)
+        fileList = getRingsFromTable(table)
         with open(targetpath + "/rings_" + str(count) + ".json", "w") as f :
             json.dump(fileList, f)
         count += 1
