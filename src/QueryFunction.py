34c57a4e7ee310ac135455753cf8c87770dc2b67
PriyankaAravindan <51697830+PriyankaAravindan@users.noreply.github.com>
2022-03-01 16:45:10 +0100

    Add files via upload

--- src/QueryFunction.py
+++ src/QueryFunction.py
@@ -1,27 +1,41 @@
 from serpapi import GoogleSearch
 def queryFunction(title, author):
-        string = '+'.join(str.split(title)) + "+" + '+'.join(str.split(author))  
-        params = {
-            "engine": "google_scholar",
-            "q": string,
-            "api_key": "255dc7fe9dd992e9626160a1d48557435eacc85f8445f9bbf0d624142d9cfd8d"
-        }
-        search = GoogleSearch(params)
-        results = search.get_dict()
-        Author_Ids = results['organic_results'][0]['publication_info']['authors']
-        Institutes = {}
-        
-        for each in Author_Ids:
-            intermediate = {}
-            params = {
-              "engine": "google_scholar_author",
-              "author_id": each['author_id'],
-              "api_key": "255dc7fe9dd992e9626160a1d48557435eacc85f8445f9bbf0d624142d9cfd8d"
-            }
-            intermediate['name'] = each['name']
+    Institutes = {}
+    string = '+'.join(str.split(title)) + "+" + '+'.join(str.split(author))  
+    params = {
+        "engine": "google_scholar",
+        "q": string,
+        "api_key": "255dc7fe9dd992e9626160a1d48557435eacc85f8445f9bbf0d624142d9cfd8d"
+    }
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    if('organic_results' in results.keys()):
+        if('publication_info' in results['organic_results'][0].keys()):
+            if('authors' in  results['organic_results'][0]['publication_info'].keys()):
+                    Author_Ids = results['organic_results'][0]['publication_info']['authors']
+    
+                    for each in Author_Ids:
+                        intermediate = {}
+                        params = {
+                          "engine": "google_scholar_author",
+                          "author_id": each['author_id'],
+                          "api_key": "255dc7fe9dd992e9626160a1d48557435eacc85f8445f9bbf0d624142d9cfd8d"
+                        }
+                        
 
-            search = GoogleSearch(params)
-            results = search.get_dict()
-            intermediate['affiliations'] = results['author']['affiliations']
-            Institutes[each['author_id']]  =  intermediate
-        return Institutes
+                        search = GoogleSearch(params)
+                        results = search.get_dict()
+                        if( 'affiliations' in results['author'].keys()):
+                            intermediate['name'] = each['name']
+                            intermediate['affiliations'] = results['author']['affiliations']
+                            Institutes[each['author_id']]  =  intermediate
+                        else:
+                            return None
+            else:
+                return None
+        else:
+            return None
+    else:
+        return None
+    return Institutes
+#profiles = results["profiles"]
