564ace4ac54a95008d17ec19835fc750797afdd6
PriyankaAravindan <51697830+PriyankaAravindan@users.noreply.github.com>
2022-03-02 04:09:40 +0100

    Add files via upload

--- src/graph_create.py
+++ src/graph_create.py
@@ -24,3 +24,21 @@ def construct_Citation_graph_Edge(articles, k, Max = 6001):
         for i in each:
             edges.append(i)
     return edges
+    
+def construct_Citation_graph(articles):
+    Citation_Graph = {}
+    Max = 6001
+    for i in range(1,Max):
+        for each in articles[i]:
+            try:
+                if (each['id'] not in Citation_Graph.keys()):
+                    Citation_Graph[each['id']] = []
+                Citation_Graph[each['id']].append(each['outCitations'])
+                for nodes in article['inCitations']:
+                    if (nodes not in Citation_Graph.keys()):
+                        Citation_Graph[nodes] = []
+                    Citation_Graph[nodes].append(each['id'])
+            except:
+                pass
+        
+    return Citation_Graph
